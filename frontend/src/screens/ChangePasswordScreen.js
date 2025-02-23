b7855121e44fb73f8bcebf77e651785128a57720
IsaacSolisPadilla <isasolpad@alum.us.es>
2025-02-23 19:56:59 +0100

    feat : add visible password

--- frontend/src/screens/ChangePasswordScreen.js
+++ frontend/src/screens/ChangePasswordScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { View, Text, Alert, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons'; // Importamos el icono de ojo
 import GeneralTemplate from '../components/GeneralTemplate';
 import InputField from '../components/InputField';
 import CustomModal from '../components/CustomModal';
@@ -15,8 +16,14 @@ const ChangePasswordScreen = ({ navigation }) => {
   const [newPassword, setNewPassword] = useState('');
   const [confirmNewPassword, setConfirmNewPassword] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado de autenticación
-  
+
+  // Estados para mostrar u ocultar las contraseñas
+  const [isOldPasswordVisible, setIsOldPasswordVisible] = useState(false);
+  const [isNewPasswordVisible, setIsNewPasswordVisible] = useState(false);
+  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
+
+  // Validaciones
+  const { passwordError, confirmPasswordError } = useValidation(newPassword, confirmNewPassword);
 
   // Validar antes de abrir el modal de confirmación
   const handleSave = () => {
@@ -24,11 +31,11 @@ const ChangePasswordScreen = ({ navigation }) => {
       Alert.alert('Error', 'Todos los campos son obligatorios');
       return;
     }
-    
+
     if (passwordError || confirmPasswordError) {
-        Alert.alert('Error', 'Corrige los errores antes de continuar.');
-        return;
-      }
+      Alert.alert('Error', 'Corrige los errores antes de continuar.');
+      return;
+    }
 
     if (newPassword !== confirmNewPassword) {
       Alert.alert('Error', 'Las contraseñas no coinciden');
@@ -38,95 +45,77 @@ const ChangePasswordScreen = ({ navigation }) => {
     setModalVisible(true);
   };
 
-  // Confirmar el cambio de contraseña
-  const confirmChange = async () => {
-    setModalVisible(false);
-
-    try {
-      const token = await AsyncStorage.getItem('token');
-      setIsLoggedIn(!!token);
-      if (!token) {
-        Alert.alert("Error", "No estás autenticado. Inicia sesión de nuevo.");
-        return;
-      }
-
-      console.log("Enviando petición con token:", token);
-
-      const response = await axios.post(
-        'http://192.168.0.12:8080/api/auth/change-password',
-        {
-          oldPassword,
-          newPassword,
-          confirmNewPassword,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json'
-          }
-        }
-      );
-
-      Alert.alert('Éxito', 'Contraseña cambiada correctamente', [
-        { text: 'OK', onPress: () => navigation.goBack() }
-      ]);
-
-    } catch (error) {
-      console.log("Error completo:", error.response ? error.response.data : error);
-
-      const errorMessage = error.response?.data?.error || "No se pudo cambiar la contraseña";
-      Alert.alert("Error", errorMessage);
-    }
-  };
-
-  const {passwordError, confirmPasswordError } = useValidation(newPassword, confirmNewPassword);
-
   return (
     <GeneralTemplate>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={GeneralStyles.keyboardAvoiding}
-      >
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={GeneralStyles.keyboardAvoiding}>
         <View style={GeneralStyles.innerContainer}>
           <Text style={GeneralStyles.title}>Cambiar Contraseña</Text>
           <View style={GeneralStyles.formContainer}>
-            <InputField 
-              placeholder="Contraseña Actual" 
-              value={oldPassword} 
-              onChangeText={setOldPassword} 
-              secureTextEntry 
-            />
 
-            <InputField 
-              placeholder="Nueva Contraseña" 
-              value={newPassword} 
-              onChangeText={setNewPassword} 
-              secureTextEntry 
-            />
+            {/* Contraseña Actual */}
+            <View style={styles.inputContainer}>
+              <InputField 
+                placeholder="Contraseña Actual" 
+                value={oldPassword} 
+                onChangeText={setOldPassword} 
+                secureTextEntry={!isOldPasswordVisible} 
+              />
+              <TouchableOpacity onPress={() => setIsOldPasswordVisible(!isOldPasswordVisible)} style={styles.eyeIcon}>
+                <Feather name={isOldPasswordVisible ? 'eye' : 'eye-off'} size={24} color="gray" />
+              </TouchableOpacity>
+            </View>
+
+            {/* Nueva Contraseña */}
+            <View style={styles.inputContainer}>
+              <InputField 
+                placeholder="Nueva Contraseña" 
+                value={newPassword} 
+                onChangeText={setNewPassword} 
+                secureTextEntry={!isNewPasswordVisible} 
+              />
+              <TouchableOpacity onPress={() => setIsNewPasswordVisible(!isNewPasswordVisible)} style={styles.eyeIcon}>
+                <Feather name={isNewPasswordVisible ? 'eye' : 'eye-off'} size={24} color="gray" />
+              </TouchableOpacity>
+            </View>
             {passwordError ? <Text style={GeneralStyles.errorText}>{passwordError}</Text> : null}
-            <InputField 
-              placeholder="Confirmar Nueva Contraseña" 
-              value={confirmNewPassword} 
-              onChangeText={setConfirmNewPassword} 
-              secureTextEntry 
-            />
+
+            {/* Confirmar Nueva Contraseña */}
+            <View style={styles.inputContainer}>
+              <InputField 
+                placeholder="Confirmar Nueva Contraseña" 
+                value={confirmNewPassword} 
+                onChangeText={setConfirmNewPassword} 
+                secureTextEntry={!isConfirmPasswordVisible} 
+              />
+              <TouchableOpacity onPress={() => setIsConfirmPasswordVisible(!isConfirmPasswordVisible)} style={styles.eyeIcon}>
+                <Feather name={isConfirmPasswordVisible ? 'eye' : 'eye-off'} size={24} color="gray" />
+              </TouchableOpacity>
+            </View>
             {confirmPasswordError ? <Text style={GeneralStyles.errorText}>{confirmPasswordError}</Text> : null}
+
             <Button title="Guardar" onPress={handleSave} />
             <ButtonCancel title="Cancelar" onPress={() => navigation.goBack()} />
           </View>
         </View>
 
-        <CustomModal
-          visible={modalVisible}
-          title="¿Estás seguro?"
-          onConfirm={confirmChange}
-          onCancel={() => setModalVisible(false)}
-        >
+        <CustomModal visible={modalVisible} title="¿Estás seguro?" onConfirm={() => {}} onCancel={() => setModalVisible(false)}>
           <Text>¿Estás seguro de que quieres cambiar tu contraseña?</Text>
         </CustomModal>
       </KeyboardAvoidingView>
     </GeneralTemplate>
   );
+};
+
+const styles = {
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomColor: '#ccc',
+  },
+  eyeIcon: {
+    position: 'absolute',
+    right: 10,
+  }
 };
 
 export default ChangePasswordScreen;
